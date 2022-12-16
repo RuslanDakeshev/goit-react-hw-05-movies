c92d76be21856a7eaaf73ef00a1fe568e62c03e8
RuslanDakeshev <bil08@outlook.com>
2022-12-17 00:46:31 +0100

    Update EventsPage.jsx

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,22 +1,22 @@
-import { fetchEvents } from "services/eventsApi"
-import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
+import { fetchEvents } from 'services/eventsApi';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export const EventsPage = () => {
-    const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([]);
 
-    useEffect(() => {
-        fetchEvents().then(setEvents)
-    }, [])
-    return (
-        events && (
-            <ul>
-                {events.map(({ name, id }) => {
-                    <li key={id}>
-                        <Link>{name}</Link>
-                    </li>
-                })}
-            </ul>
-        )
+  useEffect(() => {
+    fetchEvents().then(setEvents);
+  }, []);
+  return (
+    events && (
+      <ul>
+        {events.map(({ name, id }) => (
+          <li key={id}>
+            <Link>{name}</Link>
+          </li>
+        ))}
+      </ul>
     )
-}
+  );
+};
